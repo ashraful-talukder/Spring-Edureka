b7f5d23b97982564f0baec50c1ed8555eef56bcc
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-01-25 15:11:54 +0600

    another commit

--- SpringCoreDemo/src/co/edureka/Client.java
+++ SpringCoreDemo/src/co/edureka/Client.java
@@ -1,10 +1,15 @@
 package co.edureka;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+@SuppressWarnings("deprecation")
 public class Client {
 
 	public static void main(String[] args) {
 		
-		//object construction
+		//object construction | done by developer
 		
 		Employee eRef = new Employee();
 		eRef.setEid(101);
@@ -12,7 +17,16 @@ public class Client {
 		eRef.setEaddress("Baroi para, Kaliakair, Gazipur");
 		
 		System.out.println("Employee details: " + eRef);
-
+		
+		//Spring way | IoC
+		ClassPathResource resource = new ClassPathResource("employeebean.xml");
+		BeanFactory factory = new XmlBeanFactory((org.springframework.core.io.Resource) resource); //Spring container which shall parse XML file and construct the objects
+		
+		Employee e1 = (Employee)factory.getBean("emp1");
+		Employee e2 = factory.getBean("emp2",Employee.class);
+		
+		System.out.println("Employee 1 details: " + e1);
+		System.out.println("Employee 2 details: " + e2);
 	}
 
 }
