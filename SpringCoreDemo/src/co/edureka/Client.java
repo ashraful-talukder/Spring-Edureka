c15f82d910c7fd16537ec6df0fdfb93c53eb35f5
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-01-25 15:22:58 +0600

    another commit

--- SpringCoreDemo/src/co/edureka/Client.java
+++ SpringCoreDemo/src/co/edureka/Client.java
@@ -13,7 +13,7 @@ public class Client {
 		
 		Employee eRef = new Employee();
 		eRef.setEid(101);
-		eRef.setEname("Md. Ashraful Islam Talukder");
+		eRef.setEname("Md. Ashraful Islam Talukder"); 
 		eRef.setEaddress("Baroi para, Kaliakair, Gazipur");
 		
 		System.out.println("Employee details: " + eRef);
