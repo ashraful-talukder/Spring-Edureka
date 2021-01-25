5c7d442089b9c09f5a2338fc8ed800cb8aaf59f2
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-01-25 09:39:06 +0600

    second day of edureka

--- EdurekaSpringDemo/src/co/edureka/Client.java
+++ EdurekaSpringDemo/src/co/edureka/Client.java
@@ -1,5 +1,8 @@
 package co.edureka;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Client {
 
 	public static void main(String[] args) {
@@ -16,6 +19,15 @@ public class Client {
 		 * 2. Configure java object in an XML file
 		 * 3. Use Spring IoC container, e.g: BeanFactory or ApplicationContext to get the objects constructed by them
 		 */
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("employeebean.xml");
+		Employee e1 = (Employee)context.getBean("emp1");
+		Employee e2 = context.getBean("emp2",Employee.class);
+		
+		System.out.println("--------------------Spring IoC in Action---------------");
+		
+		System.out.println(e1);
+		System.out.println(e2);
 	}
 
 }
